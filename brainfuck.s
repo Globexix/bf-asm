e28a85b5ac1759cedd31ed29979414984331c41e
Globex <globexmc@gmail.com>
2026-03-16 01:07:58 +0100

    feat: implement + instruction and verify with strace

--- brainfuck.s
+++ brainfuck.s
@@ -11,6 +11,38 @@
 
 .section .text
 _start: 
+    # code pointer
+    # load adress of 'code' into rsi
+    lea rsi, [code]
+
+    # value pointer
+    lea rdi, [buffer]
+
+.interpreter_loop:
+    mov al, [rsi]
+    # exit if reached null terminator
+    cmp al, 0
+    je .exit
+
+    cmp al, '+'
+    je .increment
+
+    inc rsi
+    jmp .interpreter_loop
+
+.exit:
+    # print buffer to debug with strace ./brainfuck 
+    mov rax, 1          
+    mov rdi, 1         
+    lea rsi, [buffer]
+    mov rdx, 30000      
+    syscall
+
     mov rax, 60
     xor rdi, rdi
     syscall
+
+.increment:
+    inc rsi
+    inc byte ptr [rdi]
+    jmp .interpreter_loop
