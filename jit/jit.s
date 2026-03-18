1d30f87ec51658e1f8777761be1da48a2a3541d0
Globex <globexmc@gmail.com>
2026-03-18 02:27:07 +0100

    refactor(jit): modularize jit into a linkable library

--- jit/jit.s
+++ jit/jit.s
@@ -1,62 +1,8 @@
 .intel_syntax noprefix
-.global _start
-
-.section .bss
-    buffer: .skip 30000
-    code_buf: .skip 65536
-    jit_base: .skip 8
+.global bf_jit
 
 .section .text
-_start: 
-    # check argc >= 2
-    mov rax, [rsp]
-    cmp rax, 2
-    jl .exit_error
-
-    # sys open
-    mov rdi, [rsp + 16]
-    mov rax, 2
-    xor rsi, rsi
-    xor rdx, rdx
-    syscall
-
-    cmp rax, 0
-    jl .exit_error
-    # save fd
-    mov r8, rax
-
-    # sys read
-    mov rdi, r8 # fd
-    mov rax, 0
-    lea rsi, [code_buf]
-    mov rdx, 65536
-    syscall
-
-    # sys close
-    mov rdi, r8 # fd
-    mov rax, 3
-    syscall
-
-    # allocate RWX memory
-    mov rax, 9
-    mov rdi, 0
-    mov rsi, 65536
-    mov rdx, 7
-    mov r10, 0x22
-    mov r8, -1
-    mov r9, 0
-    syscall
-
-    cmp rax, 0
-    jl .exit_error
-
-    mov [jit_base], rax
-    # code pointer
-    lea rsi, [code_buf]
-    # value pointer
-    lea rdi, [buffer]
-    # jit pointer
-    mov r12, rax
+bf_jit: 
 
 .interpreter_loop:
     mov al, [rsi]
@@ -95,17 +41,15 @@ _start:
     # ret
     mov byte ptr [r12], 0xc3
 
-    mov rax, [jit_base]
+    mov rax, r13
     call rax
 
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+    xor rax, rax
+    ret
 
 .exit_error:
-    mov rax, 60
-    mov rdi, 1
-    syscall
+    mov rax, 1
+    ret
 
 .increment:
     xor rcx, rcx
